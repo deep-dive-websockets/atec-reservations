95c809f4c562f6bf5eae5499284d5dfe38b4f266
Colin Campbell <colin@colincampbell.me>
2016-04-12 05:49:43 -0500

    Clean up and improve docs

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,29 +1,45 @@
+/** This file powers most of what happens on the client (browser) **/
+
+/** These directives import classes we are going to use **/
 import { Template } from 'meteor/templating';
- 
 import { Rooms } from '../api/rooms.js';
- 
+
+/** This imports the content of our page **/
 import './body.html';
- 
+
+/**
+  * Helpers are used in the content of the
+  * page to do things like provide data
+  */
 Template.body.helpers({
+
+  // Returns all rooms and their statuses from the database
   rooms() {
     return Rooms.find({});
   },
 });
 
+/**
+  * Helpers are used in the content of the
+  * page to do things like provide data
+  */
 Template.body.events({
-  // When clicking a room
-  'click area'(event) {
-    // Prevent default browser form submit
-    event.preventDefault();
- 
-    // Get value from form element
-    const target = event.target;
-    const roomName = target.hash.substring(1);
 
+  // Fires when a room (<area>) on the map is clicked
+  'click area'(event) {
+
+    // Prevent the browser from navigating anywhere
+    event.preventDefault();
+
+    // Get the name of the room that was clicked
+    const roomName = event.target.hash.substring(1);
+
+    // Check if this room already exists in the database
     let matchingRoom = Rooms.findOne({
       name: roomName
     });
 
+    // If the room doesn't exist yet, create it
     if (typeof(matchingRoom) === 'undefined')
     {
       Rooms.insert({
@@ -31,11 +47,13 @@ Template.body.events({
         reserved: false
       });
 
+      // Point the room variable to our newly created room
       matchingRoom = Rooms.findOne({
         name: roomName
       });
     }
- 
+
+    // Toggle the room's reservation status in the database
     Rooms.update({ _id: matchingRoom._id }, {
       name: roomName,
       reserved: (!matchingRoom.reserved)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# ATEC Reservations using WebSockets
+
+## Running this code sample
+
+First, you will need to [install Meteor](https://meteor.com/install)
+
+Then simply clone this repo and run `meteor` at the terminal
+
+Meteor will inform you of where you can reach the running instance
+
+## Inspecting the code
+
+Most of the interesting bits are in [imports/ui/body.js](imports/ui/body.js)

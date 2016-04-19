5cedb95f16eff07c25d66563c374bfd1343ec75a
Jordan Caudill <jacaudill74@gmail.com>
2016-04-19 10:32:59 -0500

    Displayed seats from seats collection. Run "mongoimport -h localhost:3001 --db meteor --collection seats --file dataDump.json --jsonArray" to import the dataDump into your instance of mongo.
    The seats are displayed via flex box. Theres a vacant & reserved class you can toggle between when they reserve.

--- imports/api/rooms.js
+++
@@ -1,3 +0,0 @@
-import { Mongo } from 'meteor/mongo';
- 
-export const Rooms = new Mongo.Collection('rooms');
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,7 +2,7 @@
 
 /** These directives import classes we are going to use **/
 import { Template } from 'meteor/templating';
-import { Rooms } from '../api/rooms.js';
+import { Seats } from '../api/seats.js';
 
 /** This imports the content of our page **/
 import './body.html';
@@ -13,10 +13,10 @@ import './body.html';
   */
 Template.body.helpers({
 
-  // Returns all rooms and their statuses from the database
-  rooms() {
-    return Rooms.find({});
-  },
+  // Returns all seats and their statuses from the database
+  seats() {
+    return Seats.find({});
+  }
 });
 
 /**
@@ -26,37 +26,37 @@ Template.body.helpers({
 Template.body.events({
 
   // Fires when a room (<area>) on the map is clicked
-  'click area'(event) {
+  // 'click area'(event) {
 
-    // Prevent the browser from navigating anywhere
-    event.preventDefault();
+  //   // Prevent the browser from navigating anywhere
+  //   event.preventDefault();
 
-    // Get the name of the room that was clicked
-    const roomName = event.target.hash.substring(1);
+  //   // Get the name of the room that was clicked
+  //   const roomName = event.target.hash.substring(1);
 
-    // Check if this room already exists in the database
-    let matchingRoom = Rooms.findOne({
-      name: roomName
-    });
+  //   // Check if this room already exists in the database
+  //   let matchingRoom = Rooms.findOne({
+  //     name: roomName
+  //   });
 
-    // If the room doesn't exist yet, create it
-    if (typeof(matchingRoom) === 'undefined')
-    {
-      Rooms.insert({
-        name: roomName,
-        reserved: false
-      });
+  //   // If the room doesn't exist yet, create it
+  //   if (typeof(matchingRoom) === 'undefined')
+  //   {
+  //     Rooms.insert({
+  //       name: roomName,
+  //       reserved: false
+  //     });
 
-      // Point the room variable to our newly created room
-      matchingRoom = Rooms.findOne({
-        name: roomName
-      });
-    }
+  //     // Point the room variable to our newly created room
+  //     matchingRoom = Rooms.findOne({
+  //       name: roomName
+  //     });
+  //   }
 
-    // Toggle the room's reservation status in the database
-    Rooms.update({ _id: matchingRoom._id }, {
-      name: roomName,
-      reserved: (!matchingRoom.reserved)
-    });
-  },
+  //   // Toggle the room's reservation status in the database
+  //   Rooms.update({ _id: matchingRoom._id }, {
+  //     name: roomName,
+  //     reserved: (!matchingRoom.reserved)
+  //   });
+  // },
 });

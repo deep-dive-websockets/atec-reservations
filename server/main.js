32e2950a4e8610bf1aa5ca92cd5921021ef6aee4
Colin Campbell <colin@colincampbell.me>
2016-04-19 16:39:04 -0500

    Change from static dump to dynamically generated seat collection

--- server/main.js
+++ server/main.js
@@ -1,7 +1,27 @@
 import { Meteor } from 'meteor/meteor';
 
-import '../imports/api/seats.js';
+import { Seats } from '../imports/api/seats.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  //
 });
+
+if (Seats.find().count() === 0) {
+  const numberRows = 10;
+  const nubmerSeatsPerRow = 10;
+
+  for(let rowNumber = 0; rowNumber < numberRows; rowNumber++)
+  {
+    // Nested for loop, yeah I know; we can clean this up later
+    for(let seatNumber = 0; seatNumber < nubmerSeatsPerRow; seatNumber++)
+    {
+      const rowLetter = String.fromCharCode(65 + rowNumber);
+
+      Seats.insert({
+        "row": rowLetter,
+        "number": (seatNumber + 1),
+        "reserved": false
+      });
+    }
+  }
+}
